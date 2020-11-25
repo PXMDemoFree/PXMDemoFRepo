159498373a66ab2685f7a7a9cdcde6e0844d266a
pstreicherPXMFREE <74538935+pstreicherPXMFREE@users.noreply.github.com>
2020-11-25 14:32:28 +0100

    finally (#18)
    * finally
    
    * finally edit1
    
    * edit2
    
    Co-authored-by: Pierre Streicher <pierre.streicher@pirobase-imperia.com>

--- src/main/java/io/swagger/api/PropertTypesApiController.java
+++ src/main/java/io/swagger/api/PropertTypesApiController.java
@@ -43,7 +43,12 @@ public class PropertTypesApiController implements PropertTypesApi {
 ,@Min(0) @Max(50) @ApiParam(value = "maximum number of records to return", allowableValues = "") @Valid @RequestParam(value = "limit", required = false) Integer limit
 ) {
         // Jenkins Test
-        // Trigger SCM Poll
+        // Trigger SCM Poll//
+        String xy = "hallo";
+        String zz = "hallo";
+        if(zz == xy){
+            System.out.println("upps");
+        }
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
