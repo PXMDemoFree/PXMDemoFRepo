cbac7e6d726d952420b3c85f77b6122e500b9ab4
Pierre Streicher <pierre.streicher@pirobase-imperia.com>
2020-11-27 07:52:57 +0100

    fail?

---
+++ src/main/java/io/swagger/SonarTest.java
@@ -0,0 +1,17 @@
+/*
+ * This code is part of pirobase PIM.
+ * This code is developed by the pirobase imperia gmbh.
+ * Do not make any changes.
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * Copyright (C) pirobase imperia gmbh
+ * All Rights Reserved.
+ * ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+package io.swagger;
+/**
+ * SonarTest
+ *
+ * @since Pirobase PIM<sup>®</sup> 9.0
+ */
+public class SonarTest {
+}
