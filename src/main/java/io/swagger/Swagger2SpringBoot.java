4fc6cc6891a09cb3df91225188047d326c820ca9
pstreicherPXMFREE <74538935+pstreicherPXMFREE@users.noreply.github.com>
2020-12-01 08:44:50 +0100

    commit (#27)
    * commit
    
    * commit2
    
    * commit3

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -22,6 +22,9 @@ public class Swagger2SpringBoot implements CommandLineRunner {
 
     // comment
     public static void main(String[] args) throws Exception {
+        String c = "dssd";
+        int x = c.compareTo("nkjdddN");
+        
         new SpringApplication(Swagger2SpringBoot.class).run(args);
     }
 
